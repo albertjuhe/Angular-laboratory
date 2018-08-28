91f95ff6d34c0b6744e5d5e0b3531a0f74927b85
Albert <albertjuhe@gmail.com>
2018-08-28 16:08:25 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { LocationItemComponent } from './location-item/location-item.component';
+import { LocationListComponent } from './location-list/location-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    LocationItemComponent
+    LocationItemComponent,
+    LocationListComponent
   ],
   imports: [
     BrowserModule
